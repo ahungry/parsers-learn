b3518c760684726f4393cebc472578565a50e38d
Matthew Carter <m@ahungry.com>
2022-03-22 12:51:20 -0400

    Working map of ast form into eval'able

--- clj-vb/src/clj_vb/core.clj
+++ clj-vb/src/clj_vb/core.clj
@@ -14,3 +14,33 @@
 
 (defn main [& xs]
   (apply -main xs))
+
+(defn ast-start-rule [& xs] xs)
+(defn ast-single-rule [& xs] xs)
+(defn ast-atom [& xs] xs)
+(defn ast-logical-expr [& xs] xs)
+(defn ast-statement [& xs] xs)
+(defn ast-if-then [& xs] xs)
+(defn ast-prop-acc [& xs] xs)
+(defn ast-comparison-expr [& xs] xs)
+(defn ast-mcall [& xs] xs)
+(defn ast-class-def [& xs] xs)
+
+(defn ast-method [visibility sub name arglist & xs]
+  (prn (str "New method: " name)))
+
+
+(defn process-tree [tree]
+  (clojure.walk/prewalk-replace
+   {:startRule 'ast-start-rule
+    :singleRule 'ast-single-rule
+    :logical_expr 'ast-logical-expr
+    :statement 'ast-statement
+    :method 'ast-method
+    :class_def 'ast-class-def
+    :ifthen 'ast-if-then
+    :prop_acc 'ast-prop-acc
+    :mcall 'ast-mcall
+    :comparison_expr 'ast-comparison-expr
+    :atom 'ast-atom}
+   tree))
